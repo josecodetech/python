85d11bc59a799081d4d882afb49e68d66128653e
paninaro <paninaro@ono.com>
2017-11-01 17:37:03 +0100

    no message

---
+++ comparacionIf.py
@@ -0,0 +1,11 @@
+#SOLICITAMOS NUMEROS
+numero1=int(input("Dime el primer numero "))
+numero2=int(input("Dime el segundo numero "))
+#COMPARAMOS LOS NUMEROS
+if (numero1>numero2):
+    print("El numero ",numero1," es mayor que ",numero2)
+elif (numero1<numero2):
+    print("El numero ",numero1," es menor que ",numero2)
+else:
+    print("El numero ",numero1," es igual a ",numero2)
+print("Hemos acabado")    
