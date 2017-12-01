3d150bc813eea4ab5c140a4803a0a2f066dd7e54
paninaro <paninaro@ono.com>
2017-12-01 07:47:05 +0100

    no message

---
+++ ventana.py
@@ -0,0 +1,13 @@
+#VENTANAS CON PYTHON
+#IMPORTAMOS MODULO
+from tkinter import *
+
+ventana=Tk()
+ventana.config(bg="red")
+ventana.geometry("460x360")
+ventana.resizable(width=FALSE, height=TRUE)
+ventana.title("Ventana de Python")
+widget = Label(ventana, text='Bienvenido')
+widget.pack(expand = NO, fill=BOTH)#YES BOTH NO NONE
+ventana.mainloop()
+               
