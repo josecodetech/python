233b41b884399d45cf441c62ce79671074af0300
Jose Ojeda <paninarotico@gmail.com>
2021-10-24 09:34:03 +0200

    Actualizacion oct 21

---
+++ matematicas financieras/rentaConstanteTemporalPospagable.py
@@ -0,0 +1,17 @@
+def valorActual(capital, tiempo, interes):
+    interes = interes/100
+    valorActual = capital * ((1-(1+interes)**(-tiempo))/interes)
+    print(f"El valor actual de la renta es igual a {round(valorActual,2)}")
+    # return valorActual
+
+
+def valorFinal(capital, tiempo, interes):
+    interes = interes/100
+    valorFinal = capital * ((((1+interes)**tiempo)-1)/interes)
+    print(f"El valor final de la renta es igual a {round(valorFinal,2)}")
+    # return valorFinal
+
+
+if __name__ == "__main__":
+    valorActual(1200, 4, 11)
+    valorFinal(1200, 4, 11)
