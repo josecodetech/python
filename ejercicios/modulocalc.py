0589e6159575ab11da732502dc3b0f61d9dddfe5
Jose Ojeda <paninarotico@gmail.com>
2022-07-18 21:29:25 +0200

    Ejercicio modulos

---
+++ ejercicios/modulocalc.py
@@ -0,0 +1,17 @@
+def sumar(n1, n2):
+    return n1 + n2
+
+
+def restar(n1, n2):
+    return n1 - n2
+
+
+def multiplicar(n1, n2):
+    return n1 * n2
+
+
+def dividir(n1, n2):
+    if n2 == 0:
+        return None
+    else:
+        return n1 / n2
